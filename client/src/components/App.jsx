715d2971080d01ec96a2f525254d63956ffea813
AUnderwood9 <aunderwood797@gmail.com>
2018-03-14 01:01:39 -0500

    Attempting to select an element in another component that has been rendered to the screen using d3.js.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 import mySvg from "../../resources/images/testMap.svg"
 import Map from "./views/Map/Map"
 import mainStyle from "./index.scss"
+import D3Map from "./views/MapManipulator";
+import * as d3 from "d3";
 
 function App(props){
     console.log(mySvg);
@@ -12,6 +14,7 @@ function App(props){
             </div>
             {/* <MapPage /> */}
             <Map />
+            <D3Map />
         </ Fragment>
     );
 }
---
+++ client/src/components/views/MapManipulator.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as d3 from "d3";
+
+function MapManipulator(props){
+    let mapElement = d3.select("#maproot");
+    console.log(mapElement);    
+    return null;
+}
+
+export default MapManipulator;
